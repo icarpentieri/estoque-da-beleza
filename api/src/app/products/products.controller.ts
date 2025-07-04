f513fedca0762512a193a43f5f1cd2f4b91aa866
Isabella Carpentieri de Almeida <isacarpentieri@hotmail.com>
2025-07-04 17:19:17 -0300

    add: get by id, put, patch and delete at products controllers and services

--- api/src/app/products/products.controller.ts
+++ api/src/app/products/products.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Put,
+  ParseIntPipe,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -11,11 +23,37 @@ export class ProductsController {
     return this.productsService.findAll();
   }
 
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
   @Post()
   create(
     @Body()
     createProductDto: CreateProductDto
   ) {
     return this.productsService.create(createProductDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto
+  ) {
+    return this.productsService.update(id, updateProductDto);
+  }
+
+  @Patch(':id')
+  updatedPartial(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() UpdateProductDto: UpdateProductDto
+  ) {
+    return this.productsService.updatePartial(id, UpdateProductDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
   }
 }
--- api/src/app/products/products.service.ts
+++ api/src/app/products/products.service.ts
@@ -1,16 +1,63 @@
-import { Injectable } from '@nestjs/common'; //
+import { Injectable, NotFoundException } from '@nestjs/common'; //
 import { PrismaClient } from 'generated/prisma';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { NotFoundError } from 'rxjs';
 
 const prisma = new PrismaClient();
 
 @Injectable()
 export class ProductsService {
   async findAll() {
-    return prisma.product.findMany();
+    return prisma.product.findMany({
+      orderBy: {
+        id: 'asc',
+      },
+    });
+  }
+
+  async findOne(id: number) {
+    const product = await prisma.product.findUnique({
+      where: { id },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found.`);
+    }
+
+    return product;
   }
 
   async create(data: CreateProductDto) {
     return prisma.product.create({ data });
+  }
+
+  async update(id: number, data: UpdateProductDto) {
+    return prisma.product.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async updatePartial(id: number, data: UpdateProductDto) {
+    return prisma.product.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async remove(id: number) {
+    try {
+      await prisma.product.delete({
+        where: { id },
+      });
+
+      return { message: `Product with id ${id} deleted successfully.` };
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(`Product with id ${id} not found.`);
+      }
+      throw error;
+    }
   }
 }
